82c04408fad793c111f5abf98f40b6c6ab53e478
igorevanss <igorevans.pro@gmail.com>
2022-09-13 22:39:18 -0300

    Separação em conponentes maiores o projeto

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Main from "./Main";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+
+      <Main />
+
+      <Footer />
+    </>
+  );
+}
