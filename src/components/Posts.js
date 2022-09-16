979854389682bbdb80b1cd2b326b05e85fc84f20
igorevanss <igorevans.pro@gmail.com>
2022-09-16 10:25:33 -0300

    interação com usuário na mudança de foto e nome de usuário, implementadas

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -18,12 +18,13 @@ function Post(props) {
       <div class="fundo">
         <div class="acoes">
           <div>
-            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon onClick={()=>alert("haverá uma função em freve nesse ícone!")}
+            name="heart-outline"></ion-icon>
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
           <div>
-            <ion-icon name="bookmark-outline"></ion-icon>
+            <ion-icon onClick={()=>alert("haverá uma função em freve nesse ícone!")} name="bookmark-outline"></ion-icon>
           </div>
         </div>
 
--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,29 @@
+import React, { useState } from 'react'
+
 function UserProfile(props) {
+  const [uName, setUname] = useState(`${props.userName}`)
+  let nUserName = ''
+
+  const [uPic, setUpic] = useState(`${props.userImage}`)
+  let nUserPic = ''
+
+  function newName() {
+    nUserName = prompt('Digite o Novo Nome!')
+    setUname(nUserName)
+  }
+
+  function newPic() {
+    nUserPic = prompt('Coloque aqui o link da imagem!')
+    setUpic(nUserPic)
+  }
+
   return (
     <div class="usuario">
-      <img src={props.userImage} />
+      <img onClick={() => newPic()} src={uPic} alt={'Profile'} />
       <div class="texto">
         <strong>{props.userAt}</strong>
-        {props.userName}
-        <ion-icon name="pencil"></ion-icon>
+        {uName}
+        <ion-icon onClick={() => newName()} name="pencil"></ion-icon>
       </div>
     </div>
   )
